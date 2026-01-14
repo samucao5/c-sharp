9829678e7d37cfc0969f991aa6f383d93d561c3b
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-14 20:28:22 -0300

    Add files via upload

---
+++ dados_e_operacoes/Formatacao_de_saida/formatacao_de_saida/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace formatacao_de_saida;
+
+class Program
+{
+
+    /*
+    \n = Quebra de linha
+    \b = Backspace (delete)
+    \" = Aspas duplas
+    \a = bell (sinal sonoro)
+    \r = Carriage return
+    \t = tubulação horizontal
+    \v = tubulação vertical
+    \\ = caractere \
+    \' = caractere '
+    \" = caractere "
+    \? = caractere ? 
+    */
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\t opa,\nmundo");
+        Console.Write("\n ola, \"mundo\" ");
+        Console.Write("\n ola, \\mundo\\");
+    }
+}
