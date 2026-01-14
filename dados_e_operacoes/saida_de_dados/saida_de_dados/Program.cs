f89b3417115b095f60968aae65ef5627ec0d4929
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-14 20:02:14 -0300

    Add files via upload

---
+++ dados_e_operacoes/saida_de_dados/saida_de_dados/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace saida_de_dados;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //com exibe a mensagem com quebra de linha
+        Console.WriteLine("meu nome e samuel");
+        //exibe a mensagem sem quebra de linha
+        Console.Write(" ola, mundo ");
+        Console.Write("estou fazendo este codigo no c# ");
+    }
+}
