8b9e28d34b254544d4b5133c521ef947d4b991b9
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-16 06:13:48 -0300

    Add files via upload

---
+++ dados_e_operacoes/entrada_de_dados/entrada_do_usuario/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace entrada_do_usuario;
+
+class Program
+{
+    /*
+    leitura de um unico caractere:
+    string nome = console.read();
+
+    leitura de uma linha completa:
+    string nome = Console.ReadLine();
+
+    //formatação de saida:
+    concatenação:
+    Console.WriteLine("Nome: "+ nome + ", Idade: " + idade);
+
+    Interpolação:
+    Console.WriteLine($"Nome: {nome}, Idade: {idade}");
+
+    Indices:
+    Console.WriteLine("Nome: {0}, Idade: {1}", nome, idade);
+    
+    */
+    static void Main(string[] args)
+    {
+       Console.WriteLine("digite seu nome: ");
+       string nome = Console.ReadLine(); //retorna para uma variavel
+       Console.WriteLine($"Nome: {nome}");
+    }
+}
