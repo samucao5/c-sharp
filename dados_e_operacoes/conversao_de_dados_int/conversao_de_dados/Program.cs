8191e8a4a2fc567469db82ea9e458b56a958f098
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 16:09:52 -0300

    Add files via upload

---
+++ dados_e_operacoes/conversao_de_dados_int/conversao_de_dados/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace conversao_de_dados;
+
+class Program
+{
+    /*
+    convesoes
+    implicita e do menor para o maior 
+    conversão implicita:
+    int inteiro = 10;
+    double real = inteiro;
+
+    explicitas: e do maior para o menor 
+    conversão explicita:
+    double real = 9.8;
+    int inteiro = (int)real;
+
+    decimal > double > float > long > int > short
+    */
+
+
+    static void Main(string[] args)
+    {
+        //conversao implicita (maior -> menor)
+        // int -> long
+        int x = 10;
+        long y = x;
+        System.Console.WriteLine($"int: {x} \n long: {y}");
+
+        //float -> double
+        float f = 3.5f;
+        double d = f;
+        System.Console.WriteLine($"float: {f} \n double: {d}");
+
+        //int -> double
+        int num1 = 10;
+        double num2 = num1;
+        System.Console.WriteLine($"int: {num1} \n double: {num2}");
+
+        //explicita (maior -> menor)
+        //(tipo) valor
+        double num3 = 10.5;
+        int num4 = (int)num3;
+        System.Console.WriteLine($"double: {num3} \n int: {num4}");
+
+    }
+}
