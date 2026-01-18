6fe75b272b42c78f26557b71c79c9917440bb5bd
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 18:13:29 -0300

    Add files via upload

---
+++ dados_e_operacoes/enumeradores_e_dados_dinamicos/enumeradores_e_dados_dinamicos/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace enumeradores_e_dados_dinamicos;
+
+class Program
+{
+    /*
+    dados:
+    permanecem em variaveis que possuem um tipo
+
+    enumeradores:
+    tipo especial que representa um conjunto de valores
+
+    dados do enumerador:
+    enum Dias{segunda, quinta, sexta}
+    Dias hoje = Dias.segunda;
+
+    //tipos dinamicos:
+    oposto de tipagem forte
+
+    dynamic x = 10;
+    x = "ola, mundo";
+    
+    importante
+
+    var:
+    tipo definido em tempo de compilação
+    Depois de definido não pode mudar
+
+    dynamic:
+    tipo definido em tempo de execução
+    pode mudar para qualquer tipo
+     
+    */
+    enum estacoes{Verao, outono, inverno, primavera}
+    static void Main(string[] args)
+    {
+        estacoes estacao_atual = estacoes.Verao;
+        System.Console.WriteLine(estacao_atual);
+        System.Console.WriteLine((int)estacao_atual);
+
+        //dynamic -> sem tipo
+
+        dynamic valor = 5;
+        System.Console.WriteLine(valor);
+
+        valor = "agora e uma texto";
+        System.Console.WriteLine(valor);
+
+        valor = false;
+        System.Console.WriteLine(valor);
+    }
+}
