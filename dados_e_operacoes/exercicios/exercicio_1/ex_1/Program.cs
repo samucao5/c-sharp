f2fbbc61dbeb23f8a7758a53e7bcffe7a74dc3c1
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 20:49:49 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_1/ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ex_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite uma temperatura:\n->");
+        string temperatura = System.Console.ReadLine();
+        System.Decimal celsius = decimal.Parse(temperatura);
+        decimal fahrenheit = (celsius * 9 / 5) + 32;
+        System.Console.WriteLine($"temperatura em celsius: {celsius}\ntemperatura em fahrenheit:{System.Math.Round(fahrenheit,2)}");
+        
+    }
+}
