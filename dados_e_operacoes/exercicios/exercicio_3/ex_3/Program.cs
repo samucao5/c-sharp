39626fbf2af3d2ad698d44b98352d892c0d56859
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 21:32:49 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_3/ex_3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ex_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite uma palavra:\n->");
+        System.String palavra = (System.Console.ReadLine());
+        System.Console.WriteLine($"A palavra escrita e: {palavra.Trim()} \nO numero de caracteres: {(palavra.Trim()).Length}");
+
+    }
+}
