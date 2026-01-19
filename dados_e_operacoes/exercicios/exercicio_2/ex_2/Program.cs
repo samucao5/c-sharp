15342be44a60b7f2c35527239b8755ab0e6e20fb
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 21:14:07 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_2/ex_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+
+namespace ex_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal nota1, nota2, nota3;
+        System.Console.Write("digite a nota1:\n ->");
+        nota1 = decimal.Parse(System.Console.ReadLine());
+        System.Console.Write("digite a nota2:\n ->");
+        nota2 = decimal.Parse(System.Console.ReadLine());
+        System.Console.Write("digite a nota3:\n ->");
+        nota3 = decimal.Parse(System.Console.ReadLine());
+        decimal media = ((nota1 + nota2 + nota3)/3);
+        System.Console.WriteLine($"\n ({nota1} + {nota2} + {nota3})/3 = {System.Math.Round(media,1)}");
+
+    }
+}
