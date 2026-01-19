2a17b1e4583b9845f93ee3b63131bd520a8be387
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 21:46:51 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_4/ex_4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ex_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite o raio do circulo \n->");
+        double raio = double.Parse(System.Console.ReadLine());
+        double area = (System.Math.PI * (System.Math.Pow(raio,2)));
+        System.Console.WriteLine($"{System.Math.Round((System.Math.PI),3)} * ({raio} ^ 2) = {System.Math.Round(area,2)}");
+
+    }
+}
