ca7b740f7cde86d492d4a61e15089bdca3880404
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-18 22:12:35 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_5/ex_5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite alguma hora: \n->");
+        System.String teste = (System.Console.ReadLine()).Trim();
+        double hora = double.Parse(teste);
+        
+        int minutos = (int)(hora * 60);
+        int segundos = (int)(hora * 3600);
+
+        System.Console.WriteLine($"Horas digitadas: {hora}");
+        System.Console.WriteLine($"em minutos: {minutos}");
+        System.Console.WriteLine($"em segundos: {segundos}");
+    }
+}
