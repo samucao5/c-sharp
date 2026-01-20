c9dfd95f9774300d1ab3799a901193952c4130f1
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-20 19:41:53 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_9/ex_9/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite seu peso: \n->");
+        double peso = double.Parse(System.Console.ReadLine());
+        System.Console.Write("digite sua altura: \n->");
+        double altura = double.Parse(System.Console.ReadLine());
+
+        double IMC =  System.Math.Round((peso / (System.Math.Pow(altura, 2))),2);
+
+        System.Console.WriteLine($"IMC: {IMC}");
+    }
+}
