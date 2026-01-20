47768c78f13e883f156e21769acfb546e5bd34ea
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-20 19:41:17 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_8/ex_8/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ex_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite um valor decimal: \n->");
+        double num = double.Parse(System.Console.ReadLine());
+
+        System.Console.WriteLine($"raiz quadrada: {System.Math.Sqrt(num)}");
+        System.Console.WriteLine($"valor arredondado: {System.Math.Round(num)}");
+        System.Console.WriteLine($"valor elevado: {System.Math.Pow(num,3)}");
+    }
+}
