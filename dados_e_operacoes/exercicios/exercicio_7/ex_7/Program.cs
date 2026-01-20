7a3126448f87bfa0235def6c7fa7899d10e9eabc
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-20 19:40:49 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_7/ex_7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex_7;
+
+class Program
+{
+        
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite o seu primeiro nome: \n->");
+        System.String nome = System.Console.ReadLine().Trim();
+        int contagem_nome = nome.Length;
+        System.Console.Write("digite o seu sobrenome completo: \n->");
+        System.String sobrenome = System.Console.ReadLine().Trim();
+        int contagem_sobrenome = sobrenome.Length;
+        int soma = contagem_nome + contagem_sobrenome;
+        
+        System.Console.WriteLine($"seu nome e: {nome} {sobrenome}\n e o numero de caracteres do seu nome e: {soma}" );
+    }
+}
+
