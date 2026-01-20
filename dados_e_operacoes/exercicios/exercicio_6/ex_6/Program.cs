c8990d083209eabd7ef85914f430c69467faccc0
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-20 19:40:22 -0300

    Add files via upload

---
+++ dados_e_operacoes/exercicios/exercicio_6/ex_6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ex_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Digite a cordenada de x1: \n-> ");
+        int x1 = int.Parse(System.Console.ReadLine());
+        System.Console.Write("Digite a cordenada de y1: \n->");
+        int y1 = int.Parse(System.Console.ReadLine());
+        System.Console.Write("Digite a cordenada de x2: \n->");
+        int x2 = int.Parse(System.Console.ReadLine());
+        System.Console.Write("digite a cordenada de y2:\n->");
+        int y2 = int.Parse(System.Console.ReadLine());
+
+        int distancia_x = x1 - x2;
+        int distancia_y = y1 - y2;
+
+        System.Console.WriteLine($"a diferença entre o ponto x1: {x1} e o ponto x2: {x2} e de {distancia_x}");
+        System.Console.WriteLine($"a diferença entre o ponto y1: {y1} e o ponto y2: {y2} e de {distancia_y}");
+    }
+}
+
+
