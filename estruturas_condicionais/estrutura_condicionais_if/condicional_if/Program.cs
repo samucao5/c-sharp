9a7cdd3017454f3e094bcd1230c0442ecc0d4ab8
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-22 20:40:49 -0300

    Add files via upload

---
+++ estruturas_condicionais/estrutura_condicionais_if/condicional_if/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace condicional_if;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite sua idade:\n-> ");
+        int idade = int.Parse(System.Console.ReadLine());
+        //caso idade maior ou igual a 18, ela retorna "acesso permitido"
+        if(idade >= 18)
+        {
+            System.Console.WriteLine("acesso permitido");
+        }
+    }
+}
