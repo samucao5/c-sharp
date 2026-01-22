1c63a6e1510924e26ebee4b10dae4183724382a7
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-22 20:59:23 -0300

    Add files via upload

---
+++ estruturas_condicionais/estrutura_condicionais_else_else_if/else_else_if/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace else_else_if;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("produtos:\nbanana\nmanga\ncaderno\nuva\ndigite a sua opção\n->");
+        System.String opcao = ((System.Console.ReadLine()).Trim()).ToLower();
+        
+        if(opcao == "banana")
+        {
+            double preco = 5.99;
+            System.Console.WriteLine("banana");
+            System.Console.WriteLine($"R$ {preco}");
+        }
+        else if(opcao == "manga")
+        {
+            double preco = 4.93;
+            System.Console.WriteLine("manga");
+            System.Console.WriteLine($"R$ {preco}");
+        }
+        else if(opcao == "caderno")
+        {
+            double preco = 10.53;
+            System.Console.WriteLine("caderno");
+            System.Console.WriteLine($"R$ {preco}");
+        }
+        else if(opcao == "uva")
+        {
+            double preco = 8.99;
+            System.Console.WriteLine("uva");
+            System.Console.WriteLine($"R$ {preco}");
+        }
+        else
+        {
+            System.Console.WriteLine("produto não encontrado");
+        }
+    }
+}
