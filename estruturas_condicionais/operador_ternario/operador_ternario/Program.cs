8c0b28a4a67f67ab1b798f2522519508e7e8eaad
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-23 20:09:46 -0300

    Add files via upload

---
+++ estruturas_condicionais/operador_ternario/operador_ternario/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace operador_ternario;
+
+class Program
+{
+    /*
+    operador Ternario (?):
+    operador que ultiliza 3 argumentos
+
+    sintaxe condição ? expressão1 : expressão2;
+
+    funcionamento:
+    Resultado verdadeiro -> retorna a expressão 1
+    Resultado falso -> retorna a expressão 2
+    
+    */
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite seu nome de usuario: ");
+        System.String usuario =(System.Console.ReadLine()).Trim();
+
+        usuario = System.String.IsNullOrEmpty(usuario) ? "user555" : usuario;
+        System.Console.WriteLine($"seu de usuario e: {usuario}");
+        
+    }
+}
