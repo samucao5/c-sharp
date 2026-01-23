3b8e6313fa375cb0a796d4975c23c169a999fd21
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-23 20:36:16 -0300

    Add files via upload

---
+++ estruturas_condicionais/caso_pratico_ordenacao_de_valores/caso_pratico/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace caso_pratico;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite um numero:\n-> ");
+        double numero1 = int.Parse(System.Console.ReadLine());
+        System.Console.Write("digite um segundo numero: ");
+        double numero2 = int.Parse(System.Console.ReadLine());
+
+        if(numero1 < numero2)
+        {
+            numero1 = numero1;
+        }
+        else if(numero1 > numero2)
+        {
+            numero2 = numero1;
+        }
+        else
+        {
+            numero2 = numero2;
+        }
+
+        System.Console.Write($"maior: {numero2} \nmenor: {numero1}");
+
+    }
+}
