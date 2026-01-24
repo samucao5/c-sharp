4b608ea85d6d53c3b4cd8dd25f4ecf2f19f1e8f3
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-24 15:28:44 -0300

    Add files via upload

---
+++ estruturas_condicionais/exercicios/exercicio_9/ex_9/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ex_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.String usuario_n = "samuel";
+        System.String senha_n = "senha523";
+
+
+        System.Console.Write("digite seu nome de usuario:\n->");
+        System.String usuario = (System.Console.ReadLine().Trim()).ToLower();
+        System.Console.Write("digite sua senha:\n->");
+        System.String senha = System.Console.ReadLine().Trim();
+
+        if((usuario == usuario_n) && (senha == senha_n))
+        {
+            System.Console.WriteLine("usuario entrou");
+        }
+        else
+        {
+            System.Console.WriteLine("usuario ou senha incorreto");
+        }
+
+
+    }
+}
