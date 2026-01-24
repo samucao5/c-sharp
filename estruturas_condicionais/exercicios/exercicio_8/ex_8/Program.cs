51d0107a00fa0bced700037b3d067dceff7f73ee
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-24 15:20:27 -0300

    Add files via upload

---
+++ estruturas_condicionais/exercicios/exercicio_8/ex_8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ex_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        System.Console.Write("Digite um numero:\n->");
+        int numero = int.Parse(System.Console.ReadLine().Trim());
+        int resultado = numero % 2;
+        System.String frase = "";
+        frase = (resultado == 0) ? "ele e par" : "ele e impar";
+        System.Console.WriteLine($"o numero: {numero} {frase}");
+    }
+}
