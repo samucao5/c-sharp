ee7b2fe8c9b3df5c86c4313930d2328b6a8efdc5
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-23 21:03:24 -0300

    Add files via upload

---
+++ estruturas_condicionais/estrutura_switch_case/switch_case/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace switch_case;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite o nome do administrador:\n->");
+        System.String nome = ((System.Console.ReadLine()).Trim()).ToLower();
+
+        switch(nome)
+        {
+            case "samuel":
+                System.Console.WriteLine($"bem-vindo de volta {nome}");
+                break;
+
+            case "allan":
+                System.Console.WriteLine($"bem-vindo de volta {nome}");
+                break;
+            
+            case "isa":
+                System.Console.WriteLine($"bem-vindo de volta {nome}");
+                break;
+            
+            default:
+                System.Console.WriteLine($"este nome {nome} não existe como administrador");
+                break;
+        }
+    }
+}
