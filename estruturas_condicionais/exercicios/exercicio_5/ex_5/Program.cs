80445f96dbd995102c7cbd1bb5e056e2ce333e2e
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-24 13:24:24 -0300

    Add files via upload

---
+++ estruturas_condicionais/exercicios/exercicio_5/ex_5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite seu saldo:\n->");
+        double saldo = double.Parse(System.Console.ReadLine().Trim());
+        System.Console.Write("digite o valor do saque:\n->");
+        double saque = double.Parse(System.Console.ReadLine());
+
+        if (saldo >= saque)
+        {
+            System.Console.WriteLine($"o saque de {saque} foi feito com sucesso");
+        }
+        else
+        {
+            System.Console.WriteLine($"saldo insuficiente");
+        }
+
+    }
+}
