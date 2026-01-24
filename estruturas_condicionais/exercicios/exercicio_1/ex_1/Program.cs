a46a7fee7ee1311d9d02731e4c8963dfb506e55a
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-24 10:24:11 -0300

    Add files via upload

---
+++ estruturas_condicionais/exercicios/exercicio_1/ex_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ex_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite a temperatura atual:\n->");
+        int temperatura = int.Parse(System.Console.ReadLine().Trim());
+
+        if (temperatura < 10)
+        {
+            System.Console.WriteLine($"Temperatura atual: {temperatura}°C\n");
+            System.Console.WriteLine("Dia frio");
+        }
+        else if ((temperatura > 10) && (temperatura <= 25))
+        {
+            System.Console.WriteLine($"Temperatura atual: {temperatura}°C\n");
+            System.Console.WriteLine("Dia agradavel");
+        }
+        else
+        {
+            System.Console.Write($"temperatura atual: {temperatura}°C");
+            System.Console.WriteLine("Dia quente");
+        }
+    }
+}
