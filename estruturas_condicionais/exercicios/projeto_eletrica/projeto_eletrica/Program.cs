6761eae9dec419b2ea60ef7aabd169381e1ddcd3
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-24 16:08:01 -0300

    Add files via upload

---
+++ estruturas_condicionais/exercicios/projeto_eletrica/projeto_eletrica/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace projeto_eletrica;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("1 - calcular a resistencia\n2 - calcular a corrente\n3 - calcular a tensao\n4 - resistencia equivalente \n");
+        System.Console.Write("qual formula voce deseja calcular:\n->");
+        int opcao = int.Parse((System.Console.ReadLine()).Trim());
+
+        if (opcao == 1)
+        {
+            System.Console.Write("digite a tensão do circuito:\n->");
+            double tensao = double.Parse((System.Console.ReadLine()).Trim());
+            System.Console.Write("digite a corrente do circuito:\n->");
+            double corrente = double.Parse((System.Console.ReadLine()).Trim());
+            double resultado = (tensao / corrente);
+            System.Console.WriteLine($"R = {tensao}/{corrente} = {resultado} ohm");
+        }
+        else if (opcao == 2)
+        {
+            System.Console.Write("digite a tensão do circuito:\n->");
+            double tensao = double.Parse((System.Console.ReadLine()).Trim());
+            System.Console.Write("digite a resistencia do circuito:\n->");
+            double resistencia = double.Parse((System.Console.ReadLine()).Trim());
+            double resultado = (tensao / resistencia);
+            System.Console.WriteLine($"I = {tensao}/{resistencia} = {resultado} A");
+        }
+        else if (opcao == 3)
+        {
+            System.Console.Write("digite a corrente do circuito:\n->");
+            double corrente = double.Parse((System.Console.ReadLine()).Trim());
+            System.Console.Write("digite a resistencia do circuito:\n->");
+            double resistencia = double.Parse((System.Console.ReadLine()).Trim());
+            double resultado = (corrente * resistencia);
+            System.Console.WriteLine($"V = {corrente} * {resistencia} = {resultado} V");
+        }
+        else if (opcao == 4)
+        {
+            System.Console.Write("digite a r1:\n->");
+            double r1 = double.Parse((System.Console.ReadLine()).Trim());
+            System.Console.Write("digite a r2:\n->");
+            double r2 = double.Parse((System.Console.ReadLine()).Trim());
+            double resultado = (r1 * r2) / (r1 + r2);
+            System.Console.WriteLine($"({r1} * {r2}) / ({r1} + {r2}) = {resultado} ohm");
+        }
+        else
+        {
+            System.Console.WriteLine("opção não encontrada");
+        }
+    }
+}
