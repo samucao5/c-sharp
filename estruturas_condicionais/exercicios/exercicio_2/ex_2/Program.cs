cf5b8242c3e259d8fbc2b88366161d80ae501afe
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-24 11:00:55 -0300

    Add files via upload

---
+++ estruturas_condicionais/exercicios/exercicio_2/ex_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite um ano:\n->");
+        int ano = int.Parse(System.Console.ReadLine().Trim());
+        int ano_bissexto = ano % 4;
+        
+        if(ano_bissexto == 0)
+        {
+            System.Console.WriteLine($"{ano} e um ano bissexto");
+        }
+        else
+        {
+            System.Console.WriteLine($"{ano} não e um ano bissexto");
+        }
+    }
+}
