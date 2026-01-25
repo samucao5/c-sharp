d67cf563e8291a263ffeae53665c9072bcf1fb10
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-25 17:23:04 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/loop_while/while/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+class Program
+{
+    /*
+    O loop while e um dos 3 loops existentes na estrutura de repetição.
+
+    While significa enquanto e isso vem de o loop repetir um bloco de codigo enquanto o teste logico e verdadeiro.
+    */
+
+    static void Main(string[] args)
+    {
+        int i = 1;
+        System.Console.WriteLine("conta de 1 ate 10");
+
+        while (10 >= i)
+        {
+            System.Console.WriteLine($"valor atual {i}");
+            i++;
+        }
+    }
+}
