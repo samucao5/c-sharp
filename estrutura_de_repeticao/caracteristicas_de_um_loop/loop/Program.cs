19fd7d65fa97b29f662116664106209c27780ca8
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-25 18:41:19 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/caracteristicas_de_um_loop/loop/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace loop;
+
+class Program
+{
+    /*
+    Todos os loops possuem 3 caracteristicas em comum.
+
+    Incremento/decremento, inicialização teste lógicos.
+
+    Essas três caracteristicas permitem distinguir 3 principais loops
+    
+    */
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("contando ate 5");
+        //inicialização: Valor inicial do teste logico
+        int i = 1;
+        //teste lógico: Verificação continua de um valor
+        while(i <= 5)
+        {
+            System.Console.WriteLine(i);
+            //incrementação/Decrementação: Alteração do valor da inicialização a favor do teste lógico
+            i++;
+        }
+    }
+}
