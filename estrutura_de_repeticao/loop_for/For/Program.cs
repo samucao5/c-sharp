8ed7d2707c5c10e5403a2330726c88cfcb7f615f
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-25 20:16:18 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/loop_for/For/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace For;
+
+class Program
+{
+    /*
+    O loop For é um loop que junta 3 caracteristica de um loop, incremento, teste lógico, e inicialização numa só linha do loop
+    ex:
+    for(int i = 1;i < 5;i++){
+    }
+    */
+    static void Main(string[] args)
+    {
+        for(int i = 1;i <= 5; i++)
+        {
+            System.Console.WriteLine(i);
+        }
+
+        System.Console.WriteLine("for composto");
+
+        for(int s = 0;s <= 10; s++)
+        {
+            for(int b = 0;b <= 5; b++)
+           {
+            System.Console.Write($"nome:{b}");
+           }
+       }
+    }
+}
