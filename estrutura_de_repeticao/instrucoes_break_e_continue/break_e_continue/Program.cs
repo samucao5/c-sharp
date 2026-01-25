d1d282390b9a5b5d11352a2cc0456d8b73e36a92
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-25 18:21:46 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/instrucoes_break_e_continue/break_e_continue/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+class Program
+{
+    /*
+    um loop e formado por iterações, cada iteração é uma vez que o bloco de codigo é repetido.
+
+    Através das instruções break, e continue é possivel saltar iteração ou parar o loop atual.
+    */
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("contando ate 20 ate 1, so os numeros impares");
+        int i = 20;
+        while(1 <= i)
+        {
+            if (i%2==0)
+            {
+                i--;
+                continue;
+            }
+            System.Console.WriteLine(i);
+            i--;
+        }
+        while(true)
+        {
+            System.Console.Write("digite o nome do administrador:\n->");
+            System.String nome = (System.Console.ReadLine().Trim().ToLower());
+
+            if(nome == "samuel")
+            {
+                System.Console.WriteLine("administrador encontrado");
+                break;
+            }
+            System.Console.Clear();
+        }
+    }
+}
