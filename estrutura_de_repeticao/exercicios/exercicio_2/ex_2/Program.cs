4ed090b741703283365d27865851d0ba3653e735
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 19:57:17 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_2/ex_2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ex_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("contando de 1 a 20:");
+        for(int i = 1;i <= 20; i++)
+        {
+            System.Console.WriteLine(i);
+        }
+    }
+}
