560b95e6a51810995c362dbd8080114c08f75482
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 19:50:16 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_1/ex_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true){
+        System.Console.Write("digite uma nota de 0 - 10:\n->");
+        int nota = int.Parse(System.Console.ReadLine().Trim());
+        if((nota >= 0) && (nota <= 10))
+            {
+                System.Console.WriteLine("nota enviada com sucesso");
+                break;
+            }
+            else
+            {
+                System.Console.WriteLine("nota fora dos parametros, digite novamente");
+            }
+
+        }
+    }
+}
