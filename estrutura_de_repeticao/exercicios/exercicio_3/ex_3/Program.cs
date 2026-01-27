91b258937cb2f0a44bc6e284e1a8f7bf120e9e3f
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 20:06:34 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_3/ex_3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("contando de 1 ate 50, mas so numeros impares");
+        int i = 1;
+        while(i <= 50)
+        {
+            if ((i % 2 ) == 0)
+            {
+                i++;
+                continue;
+            };
+            System.Console.WriteLine(i);
+            i++;
+        }
+    }
+}
