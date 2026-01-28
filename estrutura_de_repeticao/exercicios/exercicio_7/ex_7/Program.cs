2435c3849e3a7efc39fae89198fcd6d48934cfd0
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 21:49:48 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_7/ex_7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ex_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = new int [10];
+        for (int i = 0;i < 10;i++)
+        {
+            System.Console.Clear();
+            System.Console.Write($"adicione um numero no vetor{i+1}:\n->");
+            numeros[i] = int.Parse(System.Console.ReadLine().Trim());
+        }
+        System.Console.WriteLine("mostrando o vetor em ordem normal:\n");
+
+        for (int i = 0;i < 10;i++)
+        {
+            System.Console.WriteLine($"numero do vetor{i + 1}:{numeros[i]}");
+        }
+
+        System.Console.WriteLine("mostrando o vetor com a ordem invertida:\n");
+        System.Array.Reverse(numeros);
+
+        for (int i = 0;i < 10;i++)
+        {
+            System.Console.WriteLine($"numero do vetor{i + 1}:{numeros[i]}");
+        }
+
+    }
+}
