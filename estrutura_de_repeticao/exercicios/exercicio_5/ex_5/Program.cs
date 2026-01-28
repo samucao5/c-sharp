b3146ed74766c94840da3272e3126db49a9f81fe
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 21:25:51 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_5/ex_5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ex_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite o numero da tabuada:\n->");
+        int valor = System.Math.Abs(int.Parse(System.Console.ReadLine().Trim()));
+        for(int i = 1;i <= 10;i++)
+        {
+            System.Console.WriteLine($"{valor} * {i} = {valor * i}");
+        }
+    }
+}
