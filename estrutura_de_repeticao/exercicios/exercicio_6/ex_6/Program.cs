e999a0bf035a0adff3958c5fe219a283c898d8f4
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 21:41:01 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_6/ex_6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = new int [5];
+        for (int i = 0;i < 5;i++)
+        {
+            System.Console.Clear();
+            System.Console.Write($"adicione um numero no vetor{i+1}:\n->");
+            numeros[i] = int.Parse(System.Console.ReadLine().Trim());
+        }
+
+        for (int i = 0;i < 5;i++)
+        {
+            System.Console.WriteLine($"numero do vetor{i + 1}:{numeros[i]}");
+        }
+    }
+}
