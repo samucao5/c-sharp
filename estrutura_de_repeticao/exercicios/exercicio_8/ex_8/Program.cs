376676881280748f763ddd5b1ac2a8c2b3f6d6fa
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-27 21:59:46 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/exercicios/exercicio_8/ex_8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = new int[51];
+
+        for(int i = 0;i <= 50;i++)
+        {
+            num[i] = i;
+        } 
+
+        for(int i = 0;i <= 50;i++)
+        {
+            System.Console.WriteLine($"vetor{i}: {num[i]}");
+
+        } 
+    }
+}
