114d4085410314982a2e11e181488e6e87febaef
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-29 22:10:45 -0300

    Add files via upload

---
+++ poo/poo_fundamentos/metodos_simples/metodos_simples/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace metodos_simples;
+
+class Program
+{
+    /*
+    Métodos são pequenos espaços num código que armazenam um bloco de codigo.
+    O bloco de código que o método armazena pode ser chamado a qualquer momento e executado
+    */
+
+    static void Main(string[] args)
+    {
+        nome();
+    }
+
+    //void -> sem retorno
+    static void nome()
+    {
+        System.Console.WriteLine("meu nome e samuel");
+    }
+}
