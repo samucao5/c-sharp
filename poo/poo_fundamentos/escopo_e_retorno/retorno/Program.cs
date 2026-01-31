d0f805a8c93fe8fac86aad6ef5bb01bcf99d257c
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-31 12:34:53 -0300

    Add files via upload

---
+++ poo/poo_fundamentos/escopo_e_retorno/retorno/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace retorno;
+
+class Program
+{
+    /*
+    cada valor num codigo possui um escopo definido
+
+    O escopo de um valor define se por quem e onde valor pode ser acessado.
+
+    */
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("digite seu nome:\n-> ");
+        System.String nome = digite_seu_nome();
+        System.Console.WriteLine($"seu nome e: {nome}");
+    }
+
+    static System.String digite_seu_nome()
+    {
+        System.String nome = System.Console.ReadLine().Trim().ToLower(); 
+        return nome;
+    }
+}
