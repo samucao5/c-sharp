a5bc4dcf61eb4218d8543717b800819d68e5543c
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-31 14:43:36 -0300

    Add files via upload

---
+++ poo/poo_fundamentos/argumento_params/Params/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Params;
+
+class Program
+{
+    /*
+    O tipo de argumento params permite-nos entrar com vários argumentos em formato de vetor.
+    Ou seja os diferentes parâmetros passados ficarão todos guardados em um vetor
+    */
+
+    static void Main(string[] args)
+    {
+        exibindo_valores(10,20,30,40);
+    }
+
+    static void exibindo_valores(params int[]numeros)
+    {
+        foreach (int n1 in numeros)
+        {
+            System.Console.WriteLine(n1);
+        }
+    }
+}
