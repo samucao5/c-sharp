617dbc05f2cfbbcd80c5d146ccad900a09a97722
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-01 13:52:58 -0300

    Add files via upload

---
+++ poo/objetos_e_classes/destrotores_de_classe/destrutores/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace destrutores;
+
+class Program
+{
+    /*
+    um método é o contrario de um método construtor da classe.
+    o método Destrutor será chamado quando um objeto deixar de existir.
+    */
+
+    static void Main(string[] args)
+    {
+        Felinos gato_1 = new Felinos("mel");
+        System.Console.WriteLine(gato_1.nome);
+
+    }
+    
+    class Felinos
+    {
+        public int vida = 10;
+        public System.String nome;
+
+        public Felinos(string n)
+        {
+            nome = n;
+        }
+
+        //para criamos destrutores so precisamos colocar um ~nome_da_classe(){}
+        ~Felinos()
+        {
+            Console.WriteLine("objeto deixou de existir");
+        }
+    
+    }   
+
+}
