2923ea7d06fc8476065faf4afe07d4947ace71e9
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-31 21:04:39 -0300

    Add files via upload

---
+++ poo/objetos_e_classes/criacao_de_classes/criacao_de_classes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace criacao_de_classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+/*classes -> modifidores da classe -> public, private
+não definido -> public
+public -> não possui restrições 
+*/
+public class classe1
+{
+    //membros da classe -> modificadores de acesso
+    //mesma coisa de public int num1 = 55;
+    int num1 = 55;
+
+    public void boa_tarde()
+    {
+        System.Console.WriteLine("boa tarde");
+    }
+}
+
