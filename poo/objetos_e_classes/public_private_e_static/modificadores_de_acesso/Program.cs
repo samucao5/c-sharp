eb2eeaf06f8b7c8de7981789cf08133f33686edf
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-01 15:43:41 -0300

    Add files via upload

---
+++ poo/objetos_e_classes/public_private_e_static/modificadores_de_acesso/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace modificadores_de_acesso;
+
+class Program
+{
+    /*
+    Introdução
+    os modificadores da classe e especificadores de acesso são termos utilizados para definir a proteção / visibilidade de um elemento
+    
+    Public -> público / sem restrições
+
+    private -> restrito à propria classe
+
+    Static -> elementos pertencem à classes e não precisam de objetos
+    todos valores dentro da static devem ser static
+
+    sintaxe
+    modificador class nome
+    {
+    especificador valor nome;
+    }
+    */
+    static void Main(string[] args)
+    {
+        Felinos gato_1 = new Felinos();
+        System.Console.WriteLine(gato_1.ex1);
+    }
+
+}
+
+public class Felinos
+{
+    public int ex1 = 0;
+    private int ex2 = 0;
+
+
+
+}
