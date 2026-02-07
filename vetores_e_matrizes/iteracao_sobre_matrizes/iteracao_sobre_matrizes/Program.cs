9890656f2b4534a6bdfe1fdac28efe3c285019cc
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-06 21:08:54 -0300

    Add files via upload

---
+++ vetores_e_matrizes/iteracao_sobre_matrizes/iteracao_sobre_matrizes/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace iteracao_sobre_matrizes;
+
+class Program
+{
+    /*
+    Iteração: Percorrer um iteravel
+    Iterável: Qualquer estrutura que pode ser percorrida
+
+    Iterar uma matriz:
+    Percorrer todos os seus elementos, um por um, para ler, modificar ou usar cada valor.
+    como? Atraves de um loop (For,While,Do-while ou foreach)
+    
+    metodos essenciais:
+    int linhas = matriz.GetLenght(0); quantidade de linhas
+    int colunas = matriz.GetLenght(1); quantidade de colunas
+    
+    Iteração com loops:
+    Loop externo -> percorrer linhas
+    Loop Interno -> percorrer colunas
+    */
+
+    static void Main(string[] args)
+    {
+        int[,] numeros = new int[3,2];
+        numeros[0,0] = 10;
+        numeros[0,1] = 5;
+        numeros[1,0] = 8;
+        numeros[1,1] = 4;
+        numeros[2,0] = 3;
+        numeros[2,1] = 1;
+
+        System.Console.WriteLine($"quantidade de linhas: {numeros.GetLength(0)}");
+        System.Console.WriteLine($"quantidade de colunas: {numeros.GetLength(1)}");
+        
+        for(int i = 0; i < numeros.GetLength(0) ;i++)
+        {
+            for(int b = 0; b < numeros.GetLength(1); b++)
+            {
+                System.Console.WriteLine($"{numeros[i,b]}");
+            }
+        }
+    }
+}
