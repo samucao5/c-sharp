7ad98dd6194079868fe7b650cbf71cf3379f58fc
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-08 13:28:17 -0300

    Add files via upload

---
+++ vetores_e_matrizes/exercicios/exercicio_2/ex_2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ex_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = new int[6];
+
+        for(int i = 0;i < numeros.Length; i++)
+        {
+            System.Console.Write($"numero{i + 1}:\n->");
+            numeros[i] = int.Parse(System.Console.ReadLine().Trim());
+            System.Console.Clear();
+        }
+
+        System.Console.WriteLine("vetor normal:");
+
+        foreach (int n in numeros)
+        {
+            System.Console.WriteLine(n);
+        }
+
+        Array.Reverse(numeros);
+
+        System.Console.WriteLine("vetor ao contrario");
+
+        foreach (int n in numeros)
+        {
+            System.Console.WriteLine(n);
+        }
+    }
+}
