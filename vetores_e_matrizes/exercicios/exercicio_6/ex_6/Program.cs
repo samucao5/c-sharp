34f2c86617e2afa26f56fdc9cff2213c44203490
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-08 16:11:26 -0300

    Add files via upload

---
+++ vetores_e_matrizes/exercicios/exercicio_6/ex_6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ex_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrizes = new int[2,4];
+        int contador = 0;
+        for(int i = 0; i < matrizes.GetLength(0); i++)
+        {
+            for(int s = 0; s < matrizes.GetLength(1); s++)
+            {
+                System.Console.Write($"digite a matrizes[{i},{s}]:\n-> ");
+                matrizes[i,s] = int.Parse(System.Console.ReadLine().Trim());
+                if(matrizes[i,s] < 0)
+                {
+                    contador += 1;
+                }
+                System.Console.Clear();
+            }
+        }
+        System.Console.WriteLine($"quantos valores negativos tem na matrizes: {contador}");
+    }
+}
