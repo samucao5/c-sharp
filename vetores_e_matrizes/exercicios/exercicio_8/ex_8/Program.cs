9845fb7eea917693e91df605a62903d6ff6e0ce7
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-08 17:37:07 -0300

    Add files via upload

---
+++ vetores_e_matrizes/exercicios/exercicio_8/ex_8/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite a quantidade de vetores:\n-> ");
+        int quantidade = int.Parse(System.Console.ReadLine().Trim());
+        int[] valores = new int [quantidade];
+        bool repetido = false;
+        for(int i = 0;i < valores.Length; i++)
+        {
+            System.Console.Write($"digite o valor {i + 1}:\n-> ");
+            valores[i] = int.Parse(System.Console.ReadLine().Trim());
+        }
+
+        for (int i = 0; i < valores.Length; i++)
+        {
+            for (int j = i + 1; j < valores.Length; j++)
+            {
+                if (valores[i] == valores[j])
+                {
+                    repetido = true;
+                }
+            }
+        }
+
+        
+        System.Console.WriteLine($"tem valores repetido: {repetido}");
+    }
+}
