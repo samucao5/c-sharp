5a14f2fdba550e32b7ffd8e2d25e898e7c3eff1d
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-08 18:21:53 -0300

    Add files via upload

---
+++ vetores_e_matrizes/exercicios/exercicio_10/ex_10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("digite a quantidade de vetor:\n-> ");
+        int quantidade = int.Parse(System.Console.ReadLine().Trim());
+        int[] vetores = new int [quantidade];
+        int contador = 0;
+        for(int i = 0; i < vetores.Length; i++)
+        {
+            System.Console.Write($"numero {i + 1}:\n-> ");
+            vetores[i] = int.Parse(System.Console.ReadLine().Trim());
+
+            if((vetores[i] >= 10) && (vetores[i] <= 20))
+            {
+                contador += 1;
+            }
+        }
+        System.Console.WriteLine($"quantidade de numeros entre 10 e 20: {contador}");
+    }
+}
