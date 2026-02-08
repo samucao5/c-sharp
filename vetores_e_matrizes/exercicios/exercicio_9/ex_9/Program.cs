3b3bde1d5e94aa2fbfcee8fcebf1e20075cba060
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-08 18:11:56 -0300

    Add files via upload

---
+++ vetores_e_matrizes/exercicios/exercicio_9/ex_9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ex_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matriz = new int[3,3];
+        int[] resultado  = new int[3];
+
+        for(int i = 0;i < matriz.GetLength(0);i++)
+        {
+            for(int d = 0;d < matriz.GetLength(1); d++)
+            {
+                System.Console.Write($"digite o valor da matriz[{i},{d}]:\n-> ");
+                matriz[i,d] = int.Parse(System.Console.ReadLine().Trim());
+                resultado[i] += matriz[i,d];
+                System.Console.Clear();
+            }
+        }
+
+        for(int c = 0;c < resultado.Length; c++)
+        {
+            System.Console.WriteLine($"soma das colunas da linha {c + 1}: {resultado[c]}");
+        }
+    }
+}
