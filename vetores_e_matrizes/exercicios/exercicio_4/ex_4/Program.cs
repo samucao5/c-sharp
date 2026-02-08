1dc8ef50cdf1df405f638a04b5fa0030d331fe00
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-08 15:12:28 -0300

    Add files via upload

---
+++ vetores_e_matrizes/exercicios/exercicio_4/ex_4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numero = {3,2,5,6};
+        int resultado = 0;
+        foreach(int n in numero)
+        {
+            if((n % 2) == 0)
+            {
+                resultado += n;
+            }
+        }
+
+        System.Console.WriteLine($"soma dos numeros pares: {resultado}");
+    }
+}
