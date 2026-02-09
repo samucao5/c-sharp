03d194792410a39fb9c270d7a1b9e431f083c366
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-09 20:09:07 -0300

    Add files via upload

---
+++ poo/heranca_polimorfismo_e_encapsulamento/ordem_de_execucao_de_contrutores/ordem_construtores/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ordem_construtores;
+
+class Program
+{
+    /*
+    Em classes derivadas os construtores de 0 argumentos serão executados entre as diferentes classes derivadas.
+    Existem uma ordem a qual esses construtores são executados.
+    */
+    static void Main(string[] args)
+    {
+        Classe_3 obj = new Classe_3();
+    }
+
+    class Classe_1
+    {
+        public Classe_1()
+        {
+            System.Console.WriteLine("Construtor da Classe_1");
+        }
+    }
+
+    class Classe_2 : Classe_1
+    {
+        public Classe_2()
+        {
+            System.Console.WriteLine("Construtor da Classe_2");
+        }
+    }
+
+    class Classe_3: Classe_2
+    {
+        public Classe_3()
+        {
+            System.Console.WriteLine("Construtor da Classe_3");
+        }
+    }
+}
