be81eee6c7089b6e5b712653421c173fe48bfb8e
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-10 09:32:41 -0300

    Add files via upload

---
+++ poo/heranca_polimorfismo_e_encapsulamento/virtual_e_override/Virtual/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Virtual;
+
+class Program
+{
+    /*
+    Métodos Virtuais são métodos que podem ser rescritos em classes derivadas através do override
+    */
+    static void Main(string[] args)
+    {
+        Classe_1 obj = new Classe_1();
+        Classe_2 obj_2 = new Classe_2();
+
+        obj.metodo();
+        obj_2.metodo();
+    }
+}
+
+class Classe_1
+{
+    public virtual void metodo()
+    {
+        System.Console.WriteLine("olá,Mundo!");
+    }
+}
+
+class Classe_2: Classe_1
+{
+    public override void metodo()
+    {
+        System.Console.WriteLine("outra coisa");
+    }
+}
