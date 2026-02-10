435f5d37996d66bfdb5d0211e7c0ed15de5f4419
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-10 10:02:22 -0300

    Add files via upload

---
+++ poo/heranca_polimorfismo_e_encapsulamento/abstract/Abstract/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Abstract;
+
+class Program
+{
+    /*
+    Classes Abstratas são classes que servirão de referencia para outras classes.
+    Não se pode instanciar objetos,e as propriedades abstratas não possuem implementação.
+    Podem ser reescritas com override
+    
+    
+    */
+    static void Main(string[] args)
+    {
+        Classe_2 obj = new Classe_2();
+        System.Console.WriteLine(obj.n1);
+        obj.M1();
+    }
+}
+
+abstract class Classe_1
+{
+    public int n1 = 10;
+//a classe que herda, tem como obrigação dar a implementação a este metodo abstrato
+    abstract public void M1(); 
+}
+
+class Classe_2 : Classe_1
+{
+    override public void M1()
+    {
+        System.Console.WriteLine("metodo preenchido");
+    }
+}
