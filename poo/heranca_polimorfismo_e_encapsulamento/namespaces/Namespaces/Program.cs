e9d3a7c4952c7d31079af32751aa4504873afebd
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-11 22:51:26 -0300

    Add files via upload

---
+++ poo/heranca_polimorfismo_e_encapsulamento/namespaces/Namespaces/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Namespaces{//diretório
+
+    class Program //nome do arquivo
+    {
+    /*
+    Um namespace é um espaço no código que armazena qualquer tipo de dado.
+    Utilizado para criar espaços e organizae o código.
+    Possui escopo.
+
+    funcoes do namespace
+    Organiza o código
+    Evita conflito de nomes
+    Define escopo
+    Permite ter classes com o mesmo nome em lugares diferentes
+    */
+        static void Main(string[] args)//metodo principal
+        {
+            // Para usar uma classe que está em outro namespace,
+            // é necessário escrever:
+            // nome_do_namespace.NomeDaClasse
+
+            // Exemplo:
+            Outro.Principal obj = new Outro.Principal();
+        }
+    }
+}
+
+namespace Outro
+{
+    //posso ter o nome das classes iguais pois eles não estão no mesmo escopo
+    class Principal 
+    {
+        public int n1;
+    }
+}
