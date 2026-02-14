9e2e88c3a1e4000a8fb3a55cf80e34354b070d42
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-14 15:41:58 -0300

    Add files via upload

---
+++ poo/heranca_polimorfismo_e_encapsulamento/generics/Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Generics;
+
+class Program
+{
+    /*
+    os tipos genericos de dados permitem-nos trabalhar com diversos tipos em algo que funcionaria apenas para um.
+    como por exemplo um argumento que aceita qualquer tipo de dado.
+
+    */
+
+    static void Main(string[] args)
+    {
+        int n1 = 20;
+        double n2 = 20.0;
+        string n3 = "20";
+        Metodo(n1);
+        Metodo(n2);
+        Metodo(n3);
+    }
+
+    public static void Metodo <Qualquer>(Qualquer res)
+    {
+        System.Console.WriteLine(res);
+    }
+}
