c823464188335d3fdf1837dbbd43b6148cfb4bc4
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-14 13:00:41 -0300

    Add files via upload

---
+++ poo/heranca_polimorfismo_e_encapsulamento/recursividade_funcao_recursivas/recursividade/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace recursividade;
+
+class Program
+{
+    /*
+    Recursividade ou métodos recursivos baseia-se em um método a ser chamado dentro dele gerando um loop
+
+    */
+    static void Main(string[] args)
+    {
+        Metodo(5);
+    }
+    static void Metodo(int n1)
+    {
+        if(n1 > 0){
+        System.Console.WriteLine("Repetiu "+n1);
+        n1--;
+        Metodo(n1);
+        }
+    }
+}
