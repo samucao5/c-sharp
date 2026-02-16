12ca4f240260d244b4be1b24f10c3c3adab4ec63
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-15 22:22:21 -0300

    Add files via upload

---
+++ algoritmos_estruturas_de_dados/algoritmos_de_ordenacao_inserction_sort/inserction_sort/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace inserction_sort;
+
+class Program
+{
+/*
+O algoritmo Inserction Sort move um elemento de cada vez para a sua posição
+Move elementos não ordenados para uma parte ordenada da lista
+Inicia no segundo elemento
+
+1 Loop
+Número de repetições
+
+2 Loop
+Move os elementos para a sua posição correta
+
+*/
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[5];
+        for(int s = 0; s < vetor.Length; s++)
+        {
+            System.Console.Write($"digite um valor para o vetor[{s + 1}]:\n->");
+            vetor[s] = int.Parse(System.Console.ReadLine().Trim());
+            System.Console.Clear();
+        }
+        InSort(vetor);
+        foreach(var item in vetor)
+        {
+            System.Console.WriteLine(item);
+        }
+
+    }
+
+    static void InSort(int[] arr)
+    {
+        for(int i = 1;i < arr.Length; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j+1] = key;
+        }
+    }
+}
