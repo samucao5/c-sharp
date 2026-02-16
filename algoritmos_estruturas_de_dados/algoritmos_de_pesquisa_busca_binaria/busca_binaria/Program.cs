d9bf334a26fd0f0cc72b9a18d056c042b61056a2
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-16 13:33:22 -0300

    Add files via upload

---
+++ algoritmos_estruturas_de_dados/algoritmos_de_pesquisa_busca_binaria/busca_binaria/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace busca_binaria;
+
+class Program
+{
+    /*
+    Busca binaria
+    O principio básico é dividir repetidamente o intervalo de busca pela metade até que o elemento desejado seja encontrado ou até que o intervalo de busca se torne vazio.
+
+    Trabalha com listas ordenadas
+    
+    Algoritmo
+    Loop While
+
+    3 condições
+    */
+    static void Main(string[] args)
+    {
+        int[] lista = {1,2,3,4,5,6,7,8,9,10};
+        System.Console.Write("Digite o numero que deseja procurar:\n->");
+        int num = int.Parse(System.Console.ReadLine().Trim());
+        int id = BuscaBinaria(lista,num);
+        System.Console.WriteLine(id);
+        
+    }
+
+    static int BuscaBinaria(int[] lista, int val)
+    {
+        int inicio = 0;
+        int fim = lista.Length -1;
+
+        while(inicio <= fim)
+        {
+            int meio = (inicio+fim)/2;
+
+            if(lista[meio] == val)
+            {
+                return lista[meio];
+            }
+            else if(lista[meio] < val)
+            {
+                inicio = meio + 1;
+            }
+            else //lista[meio] > val
+            {
+                fim = meio-1;
+            }
+        }
+        return -5;
+    }
+}
