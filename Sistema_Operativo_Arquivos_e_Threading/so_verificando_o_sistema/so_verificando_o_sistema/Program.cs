2e3ad0058a7deebf8cc2da434f999895def8b0b0
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-17 12:10:45 -0300

    Add files via upload

---
+++ Sistema_Operativo_Arquivos_e_Threading/so_verificando_o_sistema/so_verificando_o_sistema/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace so_verificando_o_sistema;
+using System.Runtime.InteropServices;
+class Program
+{
+    /*
+    É possível trabalhar com o sistema operativo em C# com a OperatingSystemClass
+
+    É possível obter diversos valores, e verificar o sistema
+    
+    obs: vai precisar chamar um classe
+    using System.Runtime.InteropServices;
+    */
+    static void Main(string[] args)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            System.Console.WriteLine("o sistema operacional e um Windows");
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            System.Console.WriteLine("o sistema operacional e um Linux");
+        }
+        else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            System.Console.WriteLine("o sistema operacional e um Mac");
+        }
+        else
+        {
+            System.Console.WriteLine("sistema operacional não encontrado");
+        }
+    }
+}
