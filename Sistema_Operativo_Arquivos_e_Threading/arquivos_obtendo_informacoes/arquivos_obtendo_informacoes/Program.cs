9343b00b59a25af35c7765753981886ba10c53be
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-17 13:42:22 -0300

    Add files via upload

---
+++ Sistema_Operativo_Arquivos_e_Threading/arquivos_obtendo_informacoes/arquivos_obtendo_informacoes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace arquivos_obtendo_informacoes;
+using System.IO;
+class Program
+{
+    /*
+    Em C# é possivel trabalhar com arquivos para diversos fins, desde manipular o interiro, criar arquivos, mover etc.
+
+    Nesta aula veremos como obter o tamanho de um arquivo e verificar se ele existe.
+    */
+    static void Main(string[] args)
+    {
+        System.String pastas = "/home/samuel/codigos/C#/dados_e_operacoes/Aula00";
+        //verifica se a pasta existe
+        bool existe_pasta = Directory.Exists(pastas);
+        System.Console.WriteLine(existe_pasta);
+        
+        var files = Directory.GetFiles(pastas, "*.*", SearchOption.AllDirectories);
+
+        foreach (string file in files)
+        {
+            var info = new FileInfo(file);
+            //mostra o nome e o tamanho do arquivo
+            System.Console.WriteLine($"{info.Name} - {info.Length} bytes");
+        }
+    }
+}
