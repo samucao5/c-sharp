927432457b46e8943d1052e6254968a2ca6bc66e
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-16 21:21:13 -0300

    Add files via upload

---
+++ algoritmos_estruturas_de_dados/estrutura_de_dados_arvores_de_pesquisa_binaria/arvore_de_pesquisa_binaria/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace arvore_de_pesquisa_binaria;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArvoreBinaria tree = new ArvoreBinaria();
+        tree.inserir(10);
+        tree.inserir(20);
+        tree.inserir(55);
+        tree.inserir(11);
+        tree.inserir(43);
+        tree.inserir(12);
+        tree.inserir(34);
+
+        tree.iniciar();
+        
+    }
+
+}
+
+class Node
+{
+    public int valor;
+    public Node esq, dir;
+
+    public Node(int v)
+    {
+        valor = v;
+        esq = dir = null;
+    }
+}
+
+class ArvoreBinaria
+{
+    public Node raiz;
+
+    public ArvoreBinaria()
+    {
+        raiz = null;
+    }
+
+    public void inserir(int val)
+    {
+        raiz = Adicionar(raiz,val);
+    }
+
+
+
+    //adicionar elementos
+    public Node Adicionar(Node raiz, int valor)
+    {
+        if(raiz == null)
+        {
+            raiz = new Node(valor);
+            return raiz;
+        }
+        if(valor < raiz.valor)
+        {
+            raiz.esq = Adicionar(raiz.esq, valor);
+        }
+        else if(valor > raiz.valor)
+        {
+            raiz.dir = Adicionar(raiz.dir, valor);    
+        }
+        return raiz;
+    }
+
+    public void Percorrer(Node raiz)
+    {
+        if(raiz!=null){
+        Percorrer(raiz.esq);
+        System.Console.Write(raiz.valor+" ");
+        Percorrer(raiz.dir);
+        //esquerda → raiz → direita
+        }
+    }
+
+    public void iniciar()
+    {
+        Percorrer(raiz);
+    }
+}
