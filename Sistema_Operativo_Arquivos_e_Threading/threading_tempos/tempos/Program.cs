c64ae1101cabdd0df786c9c6ed2f539e1cf3fdab
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-18 11:11:26 -0300

    Add files via upload

---
+++ Sistema_Operativo_Arquivos_e_Threading/threading_tempos/tempos/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace tempos;
+using System.Threading;
+class Program
+{
+    /*
+    É possível especificar um tempo em milissegundos entra cada instrução.
+
+    Para isso existe o método Thread.Sleep();
+    
+    tem que colocar o:
+    using System.Threading;
+    */
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("ola mundo");
+        Thread.Sleep(1000);//Milisegundos
+        System.Console.WriteLine("meu nome e:");
+        System.Console.Clear();
+        System.String senha = "senha123";
+        while (true)
+        {
+            System.Console.Write("digite sua senha:\n->");
+            System.String user = System.Console.ReadLine().Trim().ToLower();
+
+            if(user == senha)
+            {
+                System.Console.WriteLine("senha correta saindo do loop");
+                Thread.Sleep(3000);
+                System.Console.Clear();
+                break;
+            }
+            else
+            {
+                System.Console.WriteLine("tente novamente");
+                Thread.Sleep(3000);
+                System.Console.Clear();
+            }
+        }
+    }
+}
