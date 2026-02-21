654cd5233e689c36fc91c6e2fdd131652e2753ba
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-21 14:39:33 -0300

    Add files via upload

---
+++ design_patterns/padroes_de_criacao/padroes_de_criacao_singleton/singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace singleton;
+
+class Program
+{
+    
+    public class Singleton
+    {
+        private static Singleton instance;
+        private Singleton(){}
+
+        public static Singleton GetInstance()
+        {
+            if(instance == null){
+                instance = new Singleton();   
+            }
+            return instance;
+        }
+    } 
+
+
+
+    static void Main(string[] args)
+    {
+        Singleton obj = Singleton.GetInstance();
+        Singleton obj_2 = Singleton.GetInstance();
+        System.Console.WriteLine(obj==obj_2);
+    }
+}
