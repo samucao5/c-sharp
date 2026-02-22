9ffbca7fca65c1a388e05557784b02e641ec22d8
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-22 16:34:25 -0300

    Add files via upload

---
+++ design_patterns/padroes_de_criacao/padroes_de_criacao_factory_method/factory_method/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace factory_method;
+
+interface ModeloProduto
+{
+    void operation();
+}
+
+class Produto : ModeloProduto
+{
+    public void operation()
+    {
+        System.Console.WriteLine("Produto");
+    }
+}
+
+interface IFabrica
+{
+    ModeloProduto FactoryMethod();
+}
+
+class Fabrica : IFabrica
+{
+    public ModeloProduto FactoryMethod()
+    {
+        return new Produto();   
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IFabrica fab = new Fabrica();
+        ModeloProduto prod = fab.FactoryMethod();
+        prod.operation();
+
+    }
+}
