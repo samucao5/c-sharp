f90d5509b42f19997e12c8063a452c23733278a4
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-28 13:42:11 -0300

    Add files via upload

---
+++ design_patterns/padroes_de_criacao/padroes_de_criacao_prototype/prototype/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace prototype;
+
+abstract class Prototype
+{
+    public abstract Prototype Clone();
+}
+
+class ConcretePrototype : Prototype
+{
+    private System.String _atributo;
+    public ConcretePrototype(System.String att)
+    {
+        this._atributo = att;
+    }
+
+    public override Prototype Clone()
+    {
+        return new ConcretePrototype(this._atributo);
+    }
+
+    public void SetAtributte(System.String att)
+    {
+        this._atributo = att;
+    }
+
+    public void ShowAtributte()
+    {
+        System.Console.WriteLine("Atributo: "+_atributo);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConcretePrototype prototype = new ConcretePrototype("pular");
+        ConcretePrototype clone = (ConcretePrototype)prototype.Clone();
+        
+        clone.SetAtributte("modificado");
+        prototype.ShowAtributte();
+        clone.ShowAtributte();
+    }
+}
