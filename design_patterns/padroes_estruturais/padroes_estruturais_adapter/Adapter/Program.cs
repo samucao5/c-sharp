6ab5b069b7ef4c1d92e2f4c2c9b646c5a5b9bd2f
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-02-28 21:07:03 -0300

    Add files via upload

---
+++ design_patterns/padroes_estruturais/padroes_estruturais_adapter/Adapter/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Adapter;
+
+interface IDisp_USB
+{
+    void Ligar();
+}
+
+class Disp_USB : IDisp_USB
+{
+    public void Ligar()
+    {
+        System.Console.WriteLine("USB Ligado");
+    }
+}
+
+interface PortaHDMI
+{
+    void LigarHDMI();
+}
+
+class USBparaHDMI : PortaHDMI
+{//Adapter
+    private IDisp_USB usb;
+
+    public USBparaHDMI(IDisp_USB disp_USB)
+    {
+        usb = disp_USB;
+    }
+
+    public void LigarHDMI()
+    {
+        usb.Ligar();
+        System.Console.WriteLine("USB conectado pelo adaptador HDMI");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PortaHDMI hdmi = new USBparaHDMI(new Disp_USB());
+        hdmi.LigarHDMI();
+    }
+}
