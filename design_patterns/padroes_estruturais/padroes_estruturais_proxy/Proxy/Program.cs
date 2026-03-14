131ebbd82c017231d9e28b59e8b20ec98ede49ae
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-03-13 22:26:49 -0300

    Add files via upload

---
+++ design_patterns/padroes_estruturais/padroes_estruturais_proxy/Proxy/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Proxy;
+
+interface IObjetivo {
+    void Request();
+}
+
+class ObjetivoReal : IObjetivo
+{
+    public ObjetivoReal()
+    {
+        //Operação Cara
+        System.Console.WriteLine("operação: (Conexão ao Banco)");
+    }
+
+    public void Request()
+    {
+        System.Console.WriteLine("ObjetivoReal: Handle Request");
+    }
+}
+
+class Proxy : IObjetivo
+{
+    private ObjetivoReal _objetivoReal;
+    public void Request()
+    {
+        if(_objetivoReal == null)
+        {
+            _objetivoReal = new ObjetivoReal();
+        }
+        _objetivoReal.Request();
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IObjetivo proxy = new Proxy();
+        proxy.Request();
+    }
+}
