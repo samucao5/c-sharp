02f897a5bf804cf7f3115dabba0eaf0bcbc6df40
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-03-21 16:19:04 -0300

    Add files via upload

---
+++ design_patterns/padroes_comportamentais/padroes_comportamentais_strategy/Strategy/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Strategy;
+
+interface Strategy
+{
+    void Execute();
+}
+
+class ImpA : Strategy
+{
+    public void Execute()
+    {
+        System.Console.WriteLine("Execução A");
+    }
+}
+
+class ImpB : Strategy
+{
+    public void Execute()
+    {
+        System.Console.WriteLine("Execução B");
+    }
+}
+
+class Contexto
+{
+    private Strategy _strategy;
+    public Contexto(Strategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetStrategy(Strategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void ExecuteStrategy()
+    {
+        _strategy.Execute();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contexto context = new Contexto(new ImpA());
+        context.ExecuteStrategy();
+        context.SetStrategy(new ImpB());
+        context.ExecuteStrategy();
+
+    }
+}
