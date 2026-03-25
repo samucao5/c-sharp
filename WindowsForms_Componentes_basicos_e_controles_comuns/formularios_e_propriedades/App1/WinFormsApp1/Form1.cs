1033ccbb10f0623bb194844be081aeb668412dc7
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-03-25 20:31:35 -0300

    Add files via upload

---
+++ WindowsForms_Componentes_basicos_e_controles_comuns/formularios_e_propriedades/App1/WinFormsApp1/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class Formulario1 : Form
+    {
+        public Formulario1() //Construtor
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
