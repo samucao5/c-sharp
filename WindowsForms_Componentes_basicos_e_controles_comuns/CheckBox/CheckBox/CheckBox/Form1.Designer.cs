497ab5bde2f4c9eb4b0754c001e9b9c7161369e3
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-03-28 19:36:42 -0300

    Add files via upload

---
+++ WindowsForms_Componentes_basicos_e_controles_comuns/CheckBox/CheckBox/CheckBox/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CheckBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new System.Windows.Forms.CheckBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = SystemColors.ControlDark;
+            checkBox1.FlatStyle = FlatStyle.System;
+            checkBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(285, 97);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(91, 26);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Alternar";
+            checkBox1.UseVisualStyleBackColor = false;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(262, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Desmarcado";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private Label label1;
+    }
+}
---
+++ WindowsForms_Componentes_basicos_e_controles_comuns/CheckBox/CheckBox/CheckBox/Form1.cs
@@ -0,0 +1,32 @@
+namespace CheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label1.Text = "marcado";
+            }
+            else
+            {
+                label1.Text = "desmarcado";
+            }
+        }
+    }
+}
