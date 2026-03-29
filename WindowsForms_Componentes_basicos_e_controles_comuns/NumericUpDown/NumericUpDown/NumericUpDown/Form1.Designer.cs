b9d4b25120fd0819e828e5ad6fd10c70ad847a1a
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-03-29 15:34:46 -0300

    Add files via upload

---
+++ WindowsForms_Componentes_basicos_e_controles_comuns/NumericUpDown/NumericUpDown/NumericUpDown/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NumericUpDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(76, 131);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 0;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(212, 131);
+            button1.Name = "button1";
+            button1.Size = new Size(73, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Enviar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private Button button1;
+    }
+}
---
+++ WindowsForms_Componentes_basicos_e_controles_comuns/NumericUpDown/NumericUpDown/NumericUpDown/Form1.cs
@@ -0,0 +1,25 @@
+namespace NumericUpDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(numericUpDown1.Value.ToString());
+        }
+    }
+}
