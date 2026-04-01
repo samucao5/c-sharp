b2231a64091c003fa7a26431f3984d3c6c37891d
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-04-01 20:54:01 -0300

    Add files via upload

---
+++ WindowsForms_componetes_controles_conteiners/NotifyIcon_e_ContextMenuStrip/NotifyIcon_e_ContextMenuStrip/NotifyIcon_e_ContextMenuStrip/Form1.cs
@@ -0,0 +1,15 @@
+namespace NotifyIcon_e_ContextMenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fecharToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
