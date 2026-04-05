a35f2f2abb97cc2ee2133a3c3330955449396549
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-04-05 09:34:19 -0300

    Add files via upload

---
+++ WindowsForms_Integração_com_oBanco_de_dados/Estrutura_de_um_banco_de_dados_tabelas_dados_comandos/estrutura_banco_de_dados/estrutura_banco_de_dados/Form1.cs
@@ -0,0 +1,19 @@
+using System.Data;
+using System.Data.SQLite;
+
+namespace estrutura_banco_de_dados
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
