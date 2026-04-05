424a2345a0585d1bab7f9b55d3829c63b35314b5
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-04-04 21:40:18 -0300

    Add files via upload

---
+++ WindowsForms_Integração_com_oBanco_de_dados/Criando_do_banco_e_associacao_ao_projeto/criando_banco/criando_banco/Form1.cs
@@ -0,0 +1,18 @@
+using System.Data;
+using System.Data.SQLite;
+
+namespace criando_banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
