32324b599041d501b8901588c3ff49907d5ddd10
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-04-12 17:11:07 -0300

    Add files via upload

---
+++ mysql_e_C#/Como_deletar_com_preparated_statements/listar_banco/listar_banco/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace listar_banco
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            listView1 = new ListView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            button2 = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(291, 72);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listView1
+            // 
+            listView1.ContextMenuStrip = contextMenuStrip1;
+            listView1.Location = new Point(59, 110);
+            listView1.MultiSelect = false;
+            listView1.Name = "listView1";
+            listView1.Size = new Size(646, 297);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.ItemSelectionChanged += listView1_ItemSelectionChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(59, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Buscar no banco:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(59, 72);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "digite a busca...";
+            textBox1.Size = new Size(226, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(773, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 15);
+            label2.TabIndex = 4;
+            label2.Text = "nome contato:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(773, 134);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(773, 186);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(166, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(773, 168);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 6;
+            label3.Text = "email";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(773, 238);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(166, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(773, 220);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 15);
+            label4.TabIndex = 8;
+            label4.Text = "telefone";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(773, 279);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Atualizar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(153, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(152, 22);
+            toolStripMenuItem1.Text = "Excluir contato";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(984, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(listView1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListView listView1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private Button button2;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+    }
+}
---
+++ mysql_e_C#/Como_deletar_com_preparated_statements/listar_banco/listar_banco/Form1.cs
@@ -0,0 +1,192 @@
+using Microsoft.Win32.SafeHandles;
+using MySql.Data.MySqlClient;
+namespace listar_banco
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conn;
+        private System.String data_source = "datasource=localhost;username=root;password=@Samuel5;database=db_agenda";
+        private MySqlCommand cmd = new MySqlCommand();
+        private int id_contato_selecionado;
+        public Form1()
+        {
+            InitializeComponent();
+
+            listView1.View = View.Details;
+            listView1.LabelEdit = true;
+            listView1.AllowColumnReorder = true;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+
+            listView1.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            listView1.Columns.Add("NOME", 150, HorizontalAlignment.Left);
+            listView1.Columns.Add("E-MAIL", 150, HorizontalAlignment.Left);
+            listView1.Columns.Add("TELEFONE", 150, HorizontalAlignment.Left);
+
+            carregar_contatos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conn = new MySqlConnection(data_source);
+                cmd.Connection = conn;
+                conn.Open();
+
+                cmd.CommandText = "SELECT * FROM contato WHERE nome_contato LIKE @Buscar OR email LIKE @Buscar";
+
+                //sempre utilizar cmd.Parameters.Clear() antes de colocar um cmd.parameters.AddWithValue, se não acumula paramentros deixando o programa mais pesado depedendo do tempo de uso
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@Buscar", "%" + textBox1.Text + "%");
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                listView1.Items.Clear();
+
+                while (reader.Read())
+                {
+                    System.String[] row =
+                    {
+                        reader.GetInt32(0).ToString(),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+                    listView1.Items.Add(new ListViewItem(row));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void carregar_contatos()
+        {
+            try
+            {
+                conn = new MySqlConnection(data_source);
+                cmd.Connection = conn;
+                conn.Open();
+
+                cmd.CommandText = "SELECT * FROM contato ORDER BY id_contato ASC";
+
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                listView1.Items.Clear();
+
+                while (reader.Read())
+                {
+                    System.String[] row =
+                    {
+                        reader.GetInt32(0).ToString(),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+                    listView1.Items.Add(new ListViewItem(row));
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection itens_selecionados = listView1.SelectedItems;
+
+            foreach (ListViewItem item in itens_selecionados)
+            {
+
+                id_contato_selecionado = int.Parse(item.SubItems[0].Text);
+                textBox2.Text = item.SubItems[1].Text;
+                textBox3.Text = item.SubItems[2].Text;
+                textBox4.Text = item.SubItems[3].Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection(data_source);
+                cmd.Connection = conn;
+                conn.Open();
+
+                cmd.CommandText = "Update contato SET nome_contato=@nome, email=@email, telefone=@telefone WHERE id_contato=@id";
+
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id", id_contato_selecionado);
+                cmd.Parameters.AddWithValue("@nome", textBox2.Text);
+                cmd.Parameters.AddWithValue("@email", textBox3.Text);
+                cmd.Parameters.AddWithValue("@telefone", textBox4.Text);
+                //sempre apos os paramentros
+                cmd.ExecuteNonQuery();
+
+                //maneira mais bonita de limpar os textBox apos o uso
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                MessageBox.Show("Id selecionado = " + id_contato_selecionado, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                carregar_contatos();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conn = new MySqlConnection(data_source);
+                cmd.Connection = conn;
+                conn.Open();
+
+                DialogResult conf = MessageBox.Show("tem certeza que deseja apagar este dado? ","DELETANDO",MessageBoxButtons.YesNo,MessageBoxIcon.Exclamation);
+
+                if (conf == DialogResult.Yes)
+                {
+                    cmd.CommandText = "DELETE FROM contato WHERE id_contato=@id";
+
+                    cmd.Parameters.Clear();
+
+                    cmd.Parameters.AddWithValue("@id", id_contato_selecionado);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("deletando item id = " + id_contato_selecionado, "apagando", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+                carregar_contatos();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(" ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
