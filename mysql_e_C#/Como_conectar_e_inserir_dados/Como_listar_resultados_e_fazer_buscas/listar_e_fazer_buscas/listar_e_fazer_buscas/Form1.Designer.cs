31cd940c63f3c94fe0540d37c1c0387cbc2544fb
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-04-11 21:12:41 -0300

    Add files via upload

---
+++ mysql_e_C#/Como_conectar_e_inserir_dados/Como_listar_resultados_e_fazer_buscas/listar_e_fazer_buscas/listar_e_fazer_buscas/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace listar_e_fazer_buscas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            txtBuscar = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 82);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(776, 287);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Location = new Point(12, 53);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.PlaceholderText = "digite sua busca..";
+            txtBuscar.Size = new Size(264, 23);
+            txtBuscar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 15);
+            label1.TabIndex = 2;
+            label1.Text = "faça suas buscas:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(282, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(txtBuscar);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private TextBox txtBuscar;
+        private Label label1;
+        private Button button1;
+    }
+}
